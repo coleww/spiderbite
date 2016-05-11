ba86539b9ce86e72a63d52ba25c0576fb2ff24cc
Cole Willsea <coleww@gmail.com>
2016-05-11 09:19:09 -0700

    test the structure swapping section business. TODO: find better words for this

--- testSeq.js
+++ testSeq.js
@@ -19,8 +19,7 @@ tap.test('runs for 4 ticks and then stops, playing 4 notes', function (t) {
       ],
       mod: 1,
       current: 0,
-      tick: 0,
-      lead: true
+      tick: 0
   }])
   spiderbite.setStructure([[null]])
   spiderbite.onEnd(function () {
@@ -46,10 +45,49 @@ tap.test('runs for 4 ticks and then stops, playing 0 notes', function (t) {
       ],
       mod: 1,
       current: 0,
-      tick: 0,
-      lead: true
+      tick: 0
   }])
   spiderbite.setStructure([[null]])
+  spiderbite.onEnd(function () {
+    t.ok(true)
+  })
+  spiderbite.start()
+})
+
+tap.test('runs through different sections', function (t) {
+  t.plan(10)
+  var spiderbite = sb({bpm: 1000})
+  spiderbite.bind(true, function (data, index, array) {
+    t.equal(data, index)
+  }, [{
+      data: [
+        [[0], [1], [2], [3]]
+      ],
+      probs: [
+        [1, 1, 1, 1]
+      ],
+      nexts: [
+        [0]
+      ],
+      mod: 1,
+      current: 0,
+      tick: 0
+  },
+  {
+      data: [
+        [[0], [1], [2], [3], [4]]
+      ],
+      probs: [
+        [1, 1, 1, 1, 1]
+      ],
+      nexts: [
+        [0]
+      ],
+      mod: 1,
+      current: 0,
+      tick: 0
+  }])
+  spiderbite.setStructure([[1], [null]])
   spiderbite.onEnd(function () {
     t.ok(true)
   })
