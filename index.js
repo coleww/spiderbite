e09437a3400760fdf3934f822bbdbe0d5537ea93
Cole Willsea <coleww@gmail.com>
2016-05-15 07:25:38 -0700

    mark off private attributes

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ module.exports = function (args) {
   return {
     bpm: args.bpm || 120,
     advanceMod: args.advanceMod || 1,
-    interval: undefined,
-    counter: 0, // increments each loop
-    tick: 0, // increments each interval/beat
+    _interval: undefined,
+    _counter: 0, // increments each loop
+    _tick: 0, // increments each interval/beat
     current: 0, // which section for each inst (verse, chorus, etc.)
-    instruments: [], // the instruments, lol
-    structure: undefined, // how to jump between the larger patterns
+    _instruments: [], // the instruments, lol
+    _structure: undefined, // how to jump between the larger patterns
     onEnd: undefined, // called when the structure hits a `null`
     comparator: function (random, prob) { // called to see if an instrument should be played, can be overwritten
       return random < prob
@@ -18,19 +18,19 @@ module.exports = function (args) {
     },
     start: function () {
       // make a list, check it twice,
-      if (!this.instruments.length) throw new YouGotBitError('no data is bound')
-      if (!this.structure) throw new YouGotBitError('no structure is bound')
-      if (this.interval) throw new YouGotBitError('oops u tried to start another loop, way to go Steve Reich smdh')
-      if (!this.instruments.some(instrument => instrument.lead)) throw new YouGotBitError('a lead instrument must be bound')
+      if (!this._instruments.length) throw new YouGotBitError('no data is bound')
+      if (!this._structure) throw new YouGotBitError('no structure is bound')
+      if (this._interval) throw new YouGotBitError('oops u tried to start another loop, way to go Steve Reich smdh')
+      if (!this._instruments.some(instrument => instrument.lead)) throw new YouGotBitError('a lead instrument must be bound')
 
       // make the lead instrument be last, to simplify advancing the sequence later
-      this.instruments.sort((a, b) => a.lead ? 1 : (b.lead ? -1 : 0))
+      this._instruments.sort((a, b) => a.lead ? 1 : (b.lead ? -1 : 0))
 
-      this.interval = setInterval(() => {
+      this._interval = setInterval(() => {
         // advance the global counter
-        this.tick++
+        this._tick++
 
-        this.instruments.forEach(instrument => {
+        this._instruments.forEach(instrument => {
 
           // grab the current section for this instrument (verse, chorus, etc.)
           var section = instrument.data[this.current]
@@ -38,7 +38,7 @@ module.exports = function (args) {
           // if the section has a modulus value, see if this is it is on beat
           // i.e, mod 1: every beat, mod 2: every other beat
           // useful for creating breakdowns and bass drops
-          var onItsBeat = this.tick % (section.mod || 1) === 0
+          var onItsBeat = this._tick % (section.mod || 1) === 0
 
           // if the instrument is on it's beat, and wins the dice roll
           if (onItsBeat && this._roll(section.probs[section.current][section.tick])) {
@@ -61,12 +61,12 @@ module.exports = function (args) {
 
             // if the instrument is the lead
             if (instrument.lead) {
-              this.counter++ // advance the loop counter
+              this._counter++ // advance the loop counter
 
               // if we have played the loop some number of increments of the advanceModulus...
-              if (this.counter % this.advanceMod === 0) {
+              if (this._counter % this.advanceMod === 0) {
                 // ... pick a new section to play
-                this.current = pick(this.structure[this.current])
+                this.current = pick(this._structure[this.current])
               }
 
               // if the new section is null or some other junk
@@ -84,27 +84,27 @@ module.exports = function (args) {
     },
 
     stop: function () {
-      clearInterval(this.interval)
-      this.interval = null
+      clearInterval(this._interval)
+      this._interval = null
     },
 
     bind: function (lead, cb, data) {
 
       // if this instrument is labelled a "lead" but we already have a lead, that's a boo-boo
-      if (lead && this.instruments.some(instrument => instrument.lead)) throw new YouGotBitError('a lead instrument is already bound')
+      if (lead && this._instruments.some(instrument => instrument.lead)) throw new YouGotBitError('a lead instrument is already bound')
 
       // check to see that every existing instrument in the sequencer...
-      if (this.instruments.length) {
+      if (this._instruments.length) {
 
         // has the same number of larger patterns as the data being added...
-        if (this.instruments.some(inst => inst.data.length !== data.length)) throw new YouGotBitError('data does not match existing data')
+        if (this._instruments.some(inst => inst.data.length !== data.length)) throw new YouGotBitError('data does not match existing data')
       }
 
       // if there is a structure bound, ...
-      if (this.structure) {
+      if (this._structure) {
 
         // ... check to see that it has as many patterns as there are in the bound data
-        if (this.structure.length !== data.length) throw new YouGotBitError('data does not match existing structure')
+        if (this._structure.length !== data.length) throw new YouGotBitError('data does not match existing structure')
       }
 
       // check that the data is valid, note/prob/next-wise
@@ -123,14 +123,14 @@ module.exports = function (args) {
       if (!itIsGood) throw new YouGotBitError('data/probs internal mismatch')
 
       // if we have made it this far, push forward!
-      this.instruments.push({data: data, play: cb, lead: lead})
+      this._instruments.push({data: data, play: cb, lead: lead})
     },
 
     setStructure: function (data) {
-      if (this.instruments.length) {
-        if (this.instruments[0].data.length !== data.length) throw new YouGotBitError('structure does not match existing data')
+      if (this._instruments.length) {
+        if (this._instruments[0].data.length !== data.length) throw new YouGotBitError('structure does not match existing data')
       }
-      this.structure = data
+      this._structure = data
     }
   }
 }
